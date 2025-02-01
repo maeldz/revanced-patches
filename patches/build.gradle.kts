f1bfd7085147311b3a91e5f7c77bf83718b46c09
Ismael de Jesus <54718471+maeldz@users.noreply.github.com>
2025-02-01 04:14:44 -0300

    fix: website

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -7,7 +7,7 @@ patches {
         source = "git@github.com:maeldz/revanced-patches.git"
         author = "maeldz"
         contact = "mael.dazareia@gmail.com"
-        website = "https://github.com/maeldzp"
+        website = "https://github.com/maeldz"
         license = "GNU General Public License v3.0"
     }
 }
